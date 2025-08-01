1994125d842364110b2ae2e213bd68cecb8e036f
Fabricio da Silva Ferreira <fabricioferreira123.ff@gmail.com>
2025-08-01 13:34:09 -0300

    feat: update button in home page to redirect to authentication on click, enhancing user navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
+import { redirect } from "next/navigation";
+
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return <Button>Click me</Button>;
+  return (
+    <Button
+      onClick={() => {
+        redirect("/authentication");
+      }}
+    >
+      Entrar
+    </Button>
+  );
 }
